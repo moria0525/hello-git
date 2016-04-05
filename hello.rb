64aa4a6997bb5ef8a7b78cf222897afef6a04de8
moria0525 <moria0525@gmail.com>
2016-04-05 18:29:46 +0300

    Moved hello.rb to lib

--- hello.rb
+++
@@ -1,9 +0,0 @@
-# Default is "World"
-name = ARGV.first || "World"
-
-puts "Hello, #{name}!"
-# Default is World
-# Author: Jim Weirich (moria0525@gmail.com)
-name = ARGV.first || "World"
-
-puts "Hello, #{name}!"
